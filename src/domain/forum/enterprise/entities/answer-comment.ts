44bc101295752f5a60a5c181f11d33d6541f603d
João Victor Martins <joao.victor.gomes.martins@gmail.com>
2025-11-14 10:47:53 -0300

    refactor: comment classes

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,47 +1,15 @@
-import { Entity } from '../../../core/entities/entity'
 import { UniqueEntityId } from '../../../core/entities/unique-entity-id'
 import { Optional } from '../../../core/types/optional'
+import { ICommentProps, Comment } from './comment'
 
-interface IAnswerComment {
+interface IAnswerComment extends ICommentProps {
   answerId: UniqueEntityId
-  authorId: UniqueEntityId
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class AnswerComment extends Entity<IAnswerComment> {
+export class AnswerComment extends Comment<IAnswerComment> {
+  
   get answerId(): UniqueEntityId {
     return this.props.answerId
-  }
-
-  get authorId(): UniqueEntityId {
-    return this.props.authorId
-  }
-
-  get content(): string {
-    return this.props.content
-  }
-
-  get createdAt(): Date {
-    return this.props.createdAt
-  }
-
-  get updatedAt(): Date | undefined {
-    return this.props.updatedAt
-  }
-
-  get excerpt(): string {
-    return this.content.substring(0, 120).trimEnd().concat('...')
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
   }
 
   static create(
